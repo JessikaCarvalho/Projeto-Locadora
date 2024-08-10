a2001be873f473d5e5fd612842b22ad193f33b37
Jessika Carvalho <jessik.carvalho@hotmail.com>
2024-08-10 17:16:12 -0300

    Add files via upload
    Inserção de valores.

---
+++ SQLQuery2.sql
@@ -0,0 +1,4 @@
+Select * From Locacao
+
+insert into Locacao (LocacaoID, ClienteID, FilmeID, DataLocacao, DataDevolucaoPrevista, DataDevolucaoReal, Valor)
+values ('01', '07', '03', '10/05/2004', '15/05/2024', '14/05/2024', 20.00)
